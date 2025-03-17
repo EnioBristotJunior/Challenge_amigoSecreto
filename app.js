45f3a146bf7f94d56053fa0e3d4042875cd43e17
enio <eniobristotjunior@outlook.com>
2025-03-17 09:52:08 -0300

    Adicionar amigo ao array

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  let amigo = document.getElementById("amigo");
+  if (amigo.value != "") {
+    amigos.push(amigo.value);
+  } else {
+    alert("Por favor, insira o nome de um amigo para adicionar.");
+  }
+}
